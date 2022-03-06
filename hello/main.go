750abf9faf03c39f1a7ea548e7aa01c57c9f8e25
im <poson_hemp@yahoo.co.jp>
2022-03-06 19:46:30 +0900

    add/ if, else

--- hello/main.go
+++ hello/main.go
@@ -7,4 +7,15 @@ func main() {
 	fmt.Print("Hello, ")
 	fmt.Print("world with fmt!")
 	fmt.Print("a", 111, true)
+	// println("Hello, world!")
+
+	// if
+	x := 2
+	if x == 1 {
+		println("x is 1")
+	} else if x == 2 {
+		println("x is 2")
+	} else {
+		println("x is neither 1 nor 2.")
+	}
 }
