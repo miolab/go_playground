c07a5bee1577e8fbfe81c5c7968ee3312924426b
im <poson_hemp@yahoo.co.jp>
2022-03-07 09:08:14 +0900

    add/ switch

--- hello/main.go
+++ hello/main.go
@@ -12,10 +12,21 @@ func main() {
 	// if
 	x := 2
 	if x == 1 {
-		println("x is 1")
+		println("x is 1.")
 	} else if x == 2 {
-		println("x is 2")
+		println("x is 2.")
 	} else {
 		println("x is neither 1 nor 2.")
+	}
+
+	// switch
+	a := 0
+	switch a {
+	case 0:
+		fmt.Println("a is 0.")
+	case 1, 2:
+		fmt.Println("a is 1 or 2.")
+	default:
+		fmt.Println("defalult.")
 	}
 }
