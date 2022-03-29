d72ece562fe3c30cfe48f118b4e5407319350632
im <poson_hemp@yahoo.co.jp>
2022-03-29 19:16:47 +0900

    add/ func hello()

--- function/main.go
+++ function/main.go
@@ -2,9 +2,18 @@ package main
 
 func main() {
 	println("init main")
+
 	print_hello()
+
+	// 戻り値がある関数は変数に代入可能
+	msg := hello()
+	println(msg)
 }
 
 func print_hello() {
 	println("Hello!")
+}
+
+func hello() string {
+	return "hello"
 }
