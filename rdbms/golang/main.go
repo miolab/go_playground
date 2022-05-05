21fa65b458dff9e3951fe3f0725696916c3a3f97
im <poson_hemp@yahoo.co.jp>
2022-05-06 08:41:09 +0900

    connect golang_container to mysql_container

--- rdbms/golang/main.go
+++ rdbms/golang/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
 
 func main() {
-	fmt.Print("Hi.")
+	// Capture connection properties.
+	cfg := mysql.Config{
+		// User:   os.Getenv("DBUSER"),
+		// Passwd: os.Getenv("DBPASS"),
+		User:                 "root",
+		Passwd:               "root",
+		Net:                  "tcp",
+		Addr:                 "mysql_container:3306",
+		DBName:               "recordings",
+		AllowNativePasswords: true,
+	}
+
+	fmt.Println("Started setting...")
+
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		fmt.Println("Error!(open err)")
+		log.Fatal(err)
+	}
+
+	fmt.Println("Opened DB...")
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		fmt.Println("Error!(ping err)")
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
 }
